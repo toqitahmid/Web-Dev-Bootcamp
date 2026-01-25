2fb04547f58f153d4c473b86576d0ade98fbc212
toqitahmid <rubaiathali@gmail.com>
2026-01-25 18:24:45 +0600

    learn about js array push pop and lenth

---
+++ Milestone 3/Module 14.5/PushPop.js
@@ -0,0 +1,13 @@
+let A = [];
+
+for(let i = 0; i < 10; i++){
+    A.push(i);
+}
+
+A.push(10);
+console.log(A);
+
+for(let i = 0; i < 6; i++){
+    A.pop();
+}
+console.log(A);
---
+++ Milestone 3/Module 14.5/array.js
@@ -0,0 +1,9 @@
+const A = [10, 20, 30, 40, 50];
+// console.log(A);
+
+A[0] = 90;
+console.log(A);
+console.log(A.length);
+for(let i = 0; i < A.length; i++){
+    console.log(A[i]);
+}
