7af699b416323423dccef09b8e151290f2495607
toqitahmid <rubaiathali@gmail.com>
2026-01-28 18:54:45 +0600

    js string methods

---
+++ Milestone 3/Module 16/stringMethods.js
@@ -0,0 +1,18 @@
+// let name = "Toqi Tahmid";
+// console.log(name.toLowerCase()); //using tolowercase() method
+
+let ma = "Shiuly"
+console.log(ma.toUpperCase()); //using touppercase() method
+
+console.log(name.slice(0,4));
+//it  started from 0 index and finished at 4th index
+
+let name = "  toqi     ";
+console.log(name.trimEnd()); // using trimEnd() method
+console.log(name.length);
+
+let aim = "I want to be a porgrammer";
+console.log(aim.split(" "));  //using split() method
+
+ma = "Shiuly Khatun";
+console.log(ma.replace("Khatun", "Akter"));  //using repalce() method
