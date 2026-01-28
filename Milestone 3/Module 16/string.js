bc24bb28d25e54df3d2c1fca2e09da36136beaff
toqitahmid <rubaiathali@gmail.com>
2026-01-28 18:22:16 +0600

    string in js

--- Milestone 3/Module 16/string.js
+++ Milestone 3/Module 16/string.js
@@ -1,8 +1,11 @@
 
 let name = "Toqi Tahmid";
+let ma = new String("Shiuly")
 
 console.log(name);
 console.log(typeof(name));
+console.log(ma);
+console.log(typeof(ma));
 
 // for (let i of name)
     // console.log(i);
