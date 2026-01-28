9a7f66a13ac18227fbba448da9e23f30a998e933
toqitahmid <rubaiathali@gmail.com>
2026-01-28 15:55:48 +0600

    js string

---
+++ Milestone 3/Module 16/string.js
@@ -0,0 +1,13 @@
+
+let name = "Toqi Tahmid";
+
+console.log(name);
+console.log(typeof(name));
+
+// for (let i of name)
+    // console.log(i);
+
+console.log(name.length);
+for(let i = 0; i < name.length; i++){
+    console.log(name[i]);
+}
