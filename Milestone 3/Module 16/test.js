9bbc2fc81859563481200b07ad2e3b959e4ddd50
toqitahmid <rubaiathali@gmail.com>
2026-01-29 16:10:21 +0600

    quiz test

---
+++ Milestone 3/Module 16/test.js
@@ -0,0 +1,12 @@
+// const x = ""
+// console.log(!x.length)
+// if(!x.length){
+//     console.log("hey")
+// }
+// else{
+//     console.log("hi")
+// }
+
+const str = "Hello";
+str[0] = "h";
+console.log(str);
