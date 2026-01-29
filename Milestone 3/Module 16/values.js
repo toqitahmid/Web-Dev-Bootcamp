0bd24670746e13e407cb1038a8090737983daa99
toqitahmid <rubaiathali@gmail.com>
2026-01-29 13:22:45 +0600

    print keyvalues

---
+++ Milestone 3/Module 16/values.js
@@ -0,0 +1,11 @@
+const computer = {
+
+    brand: 'Cross',
+    price: 60000,
+    processor: 'Ryzen 5600G',
+    ssd: 'hp 512GB',
+    monitor: 'Xiomi'
+}
+
+const values = Object.values(computer);
+console.log(values);
