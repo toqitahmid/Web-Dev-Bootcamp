e0fe9b2b664512abce66e3221b09755a046db420
toqitahmid <rubaiathali@gmail.com>
2026-02-02 20:59:16 +0600

    basic structure of a function

---
+++ Milestone 3/Module 17/function.js
@@ -0,0 +1,8 @@
+function addNumbers(number1, number2) {
+  return number1 + number2;
+}
+
+let number1 = 5;
+let number2 = 15;
+
+console.log("The addition of tow number is : ", addNumbers(number1, number2));
