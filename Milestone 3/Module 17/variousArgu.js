7075564f06a1e80e8aff302ffda90e6797b94687
toqitahmid <rubaiathali@gmail.com>
2026-02-02 20:59:58 +0600

    passing multiple arguments in a function

---
+++ Milestone 3/Module 17/variousArgu.js
@@ -0,0 +1,25 @@
+function userName(){
+    console.log("Araf");
+}
+function userid(){
+
+    let id = 856294;
+    console.log(id);
+}
+function userCg(){
+
+    let cg = 3.43;
+    console.log(cg);
+}
+
+function userInfo(){
+
+    console.log("Name : ");
+    userName();
+    console.log("ID : ");
+    userid()
+    console.log("CG : ");
+    userCg()
+}
+
+userInfo();
