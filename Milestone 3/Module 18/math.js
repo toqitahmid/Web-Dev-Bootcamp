34606ff696d17b4a90f716b739a4033e75d60634
toqitahmid <rubaiathali@gmail.com>
2026-02-04 22:14:36 +0600

    some math function

---
+++ Milestone 3/Module 18/math.js
@@ -0,0 +1,7 @@
+console.log(Math.PI);
+console.log(Math.abs(-9));
+console.log(Math.round(7.004));
+console.log(Math.ceil(7.004));
+console.log(Math.floor(3.999));
+console.log(Math.sqrt(5));
+console.log(Math.random());
